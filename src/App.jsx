9cfded2e9df46bb8db959118168a5fe3ee1ea337
Aran Jothi <aran.jothi@gmail.com>
2025-08-21 13:08:28 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -32,12 +32,22 @@ function App() {
         </div>
       </div>
       ) : currentscreen === "join" ? ( /* current screen is join? */
-        <div className = "backbutton">
+        <div className = "joincontainer">
           <button onClick = {back}>Back</button>
+          <div className = "useforcenterscreen">
+            <div className = "entercodetext">Enter Code:</div>
+            <input type = "text" placeholder = "e.g., 123456" />
+            <button style = {{position: 'relative', top: '25px', left: 'auto'}}>Join</button>
+          </div>
         </div>
       ) : currentscreen === "create" ? (
-        <div className = "backbutton">
+        <div className = "joincontainer">
           <button onClick = {back}>Back</button>
+          <div className = "useforcenterscreen">
+
+            
+            
+          </div>
         </div>
       ) : (
         <div></div>
