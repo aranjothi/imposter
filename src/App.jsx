2dda87518dc74d04e227288f2075e4cfb6164287
Aran Jothi <aran.jothi@gmail.com>
2025-08-20 22:49:37 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,26 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [currentscreen, setcurrentscreen] = useState("main");
 
   function joingame() {
     console.log("Join Game")
+    setcurrentscreen("join");
+  }
+
+  function back() {
+    setcurrentscreen("main");
   }
 
   function creategame() {
     console.log("Create Game")
+    setcurrentscreen("create");
   }
  
   return (
     <>
     <div className = "entirecontainer">
+      {currentscreen === "main" ? ( /* current screen is main? */
       <div className = "impostertitlecontainer">
         <h1 style = {{marginBottom: '30px'}}>Imposter</h1>
         <div className = "joinandcreatebuttons">
@@ -24,9 +31,21 @@ function App() {
           <button onClick = {creategame}>Create Game</button>
         </div>
       </div>
-      </div>
+      ) : currentscreen === "join" ? ( /* current screen is join? */
+        <div className = "backbutton">
+          <button onClick = {back}>Back</button>
+        </div>
+      ) : currentscreen === "create" ? (
+        <div className = "backbutton">
+          <button onClick = {back}>Back</button>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
     </>
-  )
-}
+    )
+  }
+
 
 export default App
